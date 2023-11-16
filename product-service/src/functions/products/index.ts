bc247e41ad423192df4c9ff1eb4ac04fd0bb3b16
Jika1995 <jikulina2016@gmail.com>
2023-11-16 14:36:32 +0600

    getproducts wih serverless

---
+++ product-service/src/functions/products/index.ts
@@ -0,0 +1,15 @@
+
+import { handlerPath } from '@libs/handler-resolver';
+
+export default {
+  handler: `${handlerPath(__dirname)}/getProductsList.main`,
+  events: [
+    {
+      http: {
+        method: 'get',
+        path: 'products',
+        cors: true
+      },
+    },
+  ],
+};
